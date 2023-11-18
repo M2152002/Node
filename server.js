17e445a29802df61d15290ca83e598eacee27e13
MAHALAKSHMI G <89649311+M2152002@users.noreply.github.com>
2023-11-18 18:43:41 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+  console.log('Maha');
+
+
+  res.end('Maha\n');
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

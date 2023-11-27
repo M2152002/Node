c11bca7e1cb3e69cdb06b19f3c4b22d01696de43
MAHALAKSHMI G <89649311+M2152002@users.noreply.github.com>
2023-11-27 17:28:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express  = require('express');  // ctrl + click
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);

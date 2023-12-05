b8c22b373a33638e5dc72e1e538f0966992ddd65
MAHALAKSHMI G <89649311+M2152002@users.noreply.github.com>
2023-12-05 18:02:09 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,28 @@
+
+const Sequelize =require('sequelize');
+
+const sequelize =require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;

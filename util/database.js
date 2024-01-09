ada59fc3beb219ecd8fa38a037a3753fc35ca117
MAHALAKSHMI G <89649311+M2152002@users.noreply.github.com>
2024-01-09 11:40:54 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,8 +1,1 @@
-const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expense-tracker", "root", "MySQL#1234", {
-    dialect : "mysql",
-    host : "localhost"
-});
-
-module.exports = sequelize;
